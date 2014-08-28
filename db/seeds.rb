c9e49d120282f5376688bc25dabf34772e1d1718
Priyank <priyank1985gupta@rediffmail.com>
2014-08-28 18:46:49 +0530

    authentication handeled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+@user = User.new email: 'ims@xebia.com'
+@user.password = 'pass123654'
+@user.password_confirmation = 'pass123654'
+@user.save!
